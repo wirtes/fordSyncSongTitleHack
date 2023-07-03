001dc37d74517c9777e2e7ead4ae69df3ae5aa90
Al Wirtes <wirtes@gmail.com>
2023-07-03 10:03:40 -0600

    initial commit

---
+++ fordHack.py
@@ -0,0 +1,43 @@
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+
+import os
+from music_tag import load_file
+
+def update_metadata(directory):
+    # Iterate through all files and directories
+    for root, dirs, files in os.walk(directory):
+        # Exclude directories starting with "."
+        dirs[:] = [d for d in dirs if not d.startswith('.')]
+        # Exclude files starting with "."
+        files[:] = [f for f in files if not f.startswith('.')]
+
+        for filename in files:
+            if filename.endswith('.mp3') or filename.endswith('.m4a'):
+                file_path = os.path.join(root, filename)
+                try:
+                    audio = load_file(file_path)
+
+                    # Get the existing title and track number
+                    title = audio["title"]
+                    track_number = audio["tracknumber"]
+                    comment = str(audio["comment"])
+
+                    # Add a two-digit track number to the beginning of the title
+                    new_track_number = str(int(track_number)).zfill(2)
+                    new_title = f"{new_track_number} - {title}"
+                    new_comment = "Ford Sync Sucks"
+                    # print(comment, new_comment)
+                    if comment != new_comment:
+                        # Update the title in the metadata
+                        audio["title"] = new_title
+                        audio["comment"] = new_comment
+                        # Save the changes
+                        audio.save()
+                        print(f"Updated metadata for {file_path}")
+                except Exception as e:
+                    print(f"Failed to update metadata for {file_path}: {e}")
+
+
+# Provide the directory path where the MP3 files are located
+directory_path = "/Volumes/USB64/Albums"
+update_metadata(directory_path)
