308fb449b397aaa51a7a7f50d2da31cd66f6a674
Al Wirtes <wirtes@gmail.com>
2023-07-03 10:46:22 -0600

    track processed update

--- fordHack.py
+++ fordHack.py
@@ -4,15 +4,22 @@ import os
 from music_tag import load_file
 
 def update_metadata(directory):
+    # Track files processed to give feedback
+    tracks_processed = 0
     # Iterate through all files and directories
     for root, dirs, files in os.walk(directory):
+
         # Exclude directories starting with "."
         dirs[:] = [d for d in dirs if not d.startswith('.')]
         # Exclude files starting with "."
         files[:] = [f for f in files if not f.startswith('.')]
 
         for filename in files:
+            tracks_processed += 1
             if filename.endswith('.mp3') or filename.endswith('.m4a'):
+                # Print a dot every 50 tracks
+                if tracks_processed % 50 == 0:
+                    print("Processed " + str(tracks_processed) + " tracks")
                 file_path = os.path.join(root, filename)
                 try:
                     audio = load_file(file_path)
@@ -39,5 +46,5 @@ def update_metadata(directory):
 
 
 # Provide the directory path where the MP3 files are located
-directory_path = "/Volumes/USB64/Albums"
+directory_path = "/Volumes/USB/Albums"
 update_metadata(directory_path)
